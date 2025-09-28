02937051a81012a2b2a7d2241bff670d4e88b806
catnipglitch <192207690+catnipglitch@users.noreply.github.com>
2025-09-28 23:59:58 +0900

    初期設定と実装の追加、不要なファイルの削除

--- src/gradio_hello.py
+++
@@ -1,9 +0,0 @@
-import gradio as gr
-
-def hello():
-    return "Hello world"
-
-demo = gr.Interface(fn=hello, inputs=None, outputs="text", title="Hello World Demo")
-
-if __name__ == "__main__":
-    demo.launch()
